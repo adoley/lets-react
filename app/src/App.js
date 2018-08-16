6408049367368067304b0e0a37a2650d232889fe
Apollo Doley <adoley@c02p26a9g3qc.dlink>
2018-08-16 20:01:48 +0530

    added proptypes

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>hello react </h1>
+          <h1>{this.props.a} + {this.props.b} = {this.props.a + this.props.b}</h1>
       </div>
     );
   }
+
 }
+App.propTypes={
+    a: PropTypes.number,
+    b: PropTypes.number
+};
 
 export default App;
